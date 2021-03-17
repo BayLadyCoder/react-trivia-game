b9ac2d8551a51f5e736a83c474830befd034ca8a
BayLadyCoder <rachada_32@hotmail.com>
2021-03-17 00:27:31 -0400

    Updates

---
+++ precache-manifest.2b7a530ba6b75bc1e9bdd8ff94c4baf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32cda74fe3fa3d0e37c8fc7722b4c98a",
+    "url": "/react-trivia-game/index.html"
+  },
+  {
+    "revision": "5ee2341a0cd4f2085016",
+    "url": "/react-trivia-game/static/css/main.8142b031.chunk.css"
+  },
+  {
+    "revision": "1abb47b2a778a5703905",
+    "url": "/react-trivia-game/static/js/2.d279692d.chunk.js"
+  },
+  {
+    "revision": "5ee2341a0cd4f2085016",
+    "url": "/react-trivia-game/static/js/main.848ae23e.chunk.js"
+  },
+  {
+    "revision": "09f37e7819ef312bffda",
+    "url": "/react-trivia-game/static/js/runtime~main.45dbfa25.js"
+  }
+]);
